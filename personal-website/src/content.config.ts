e5c359e4ece10dcf9bc04441c66d050d9c663c79
Beehavesh <bhaveshapathak@gmail.com>
2026-08-13 22:55:11 -0700

    fleshed out the remainder

---
+++ personal-website/src/content.config.ts
@@ -0,0 +1,62 @@
+import { glob, file } from 'astro/loaders';
+import { defineCollection } from 'astro/content/config';
+import { z } from 'astro/zod'
+
+const projects = defineCollection({
+    loader: glob({pattern: "src/content/projects/**/*.md"}),
+    schema: z.object({
+        id: z.number(),
+        title: z.string().max(50),
+        tools: z.preprocess(
+            (val) => (Array.isArray(val) ? val : [val]),
+            z.array(z.enum(["TypeScript", "LIVE", "HTML", "JavaScript", "React Native", "Backend API", "Node.js", "Docker", "Tailwind"]))), // change categories here
+        year: z.string().max(4),
+        liveSite: z.url().optional(),
+        github: z.url().optional(),
+        description: z.string().max(350),
+        isFeatured: z.boolean(),
+        isDraft: z.boolean()
+    })
+});
+
+const blog = defineCollection({
+    loader: glob({pattern: "src/content/blog/**/*.md"}),
+    schema: z.object({
+        id: z.number(),
+        slug: z.string().max(50),
+        title: z.string().max(50),
+        publishedDate: z.date(),
+        category: z.enum(["systems", "ai", "productivity"]), // change and add blog categories here
+        readingTime: z.number().optional(),
+        isDraft: z.boolean()
+    })
+})
+
+const experience = defineCollection({
+    loader: file("src/content/resume/experience.yaml"),
+    schema: z.object({
+        title: z.string().max(70),
+        timeline: z.string().max(15),
+        description: z.string().max(500)
+    })
+})
+
+const education = defineCollection({
+    loader: file("src/content/resume/education.yaml"),
+    schema: z.object({
+        title: z.string().max(70),
+        timeline: z.string().max(15),
+        school: z.string().max(70)
+    })
+})
+
+const skillsAndTools = defineCollection({
+    loader: file("src/content/skills-and-tools/skillsAndTools.yaml"),
+    schema: z.object({
+        title: z.string().max(70),
+        items: z.array(z.string())
+    })
+})
+
+
+export const collections = { projects, blog, experience, education, skillsAndTools };
